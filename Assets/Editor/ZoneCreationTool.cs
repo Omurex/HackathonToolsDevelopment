3c19ba68916e40c1952e3f4cb01a9848132a2283
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-05-23 22:24:19 -0400

    Got proper color changing and zone addition from rectangle select

--- Assets/Editor/ZoneCreationTool.cs
+++ Assets/Editor/ZoneCreationTool.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0113d6d9ac93dde52d1abbbce1b89e381ee76853cc523b8b774ce092cd08a7e8
-size 7733
+oid sha256:c32f0feef06a4068d56329ac446ed022f89001fd61ee734ce3826d564abbb8b2
+size 8516
