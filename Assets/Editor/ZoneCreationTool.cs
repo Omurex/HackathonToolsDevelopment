0ac3d9b63ea97a15cd9b4fd7a0bed63ae15217b6
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-05-24 11:09:30 -0400

    Refactored slightly for more modular selection

--- Assets/Editor/ZoneCreationTool.cs
+++ Assets/Editor/ZoneCreationTool.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de9a87cac5373ea1079716c3542eed534251fafd250159da0a08b95bdf454c80
-size 10419
+oid sha256:8272f9578ba3f6e3cfc960675348487e136c636dd53ae0f338791f281cc8c786
+size 10400
