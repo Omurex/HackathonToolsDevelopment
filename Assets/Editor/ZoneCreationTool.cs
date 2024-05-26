1eb03d9d6829eeb5a88011851c9b89918e40f967
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-05-26 16:17:52 -0400

    Added option to delete all zones

--- Assets/Editor/ZoneCreationTool.cs
+++ Assets/Editor/ZoneCreationTool.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f52b3548d801f0159616f2561e7782fe7e7ad1a57d987fd9241647211c7e440b
-size 16139
+oid sha256:6bfd6c03bdde55d95f791704d4c4c287578ae5ff0fbe70a45555c0a2ab563ca1
+size 16795
