a9185775d2b15d7de41d5ef9e7226c84e3b1e872
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-06-02 16:18:02 -0400

    Got borders looking clean

--- Assets/Scripts/Zone.cs
+++ Assets/Scripts/Zone.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ca2c48ca64e4c1b94c125d9b822c736f62ad9648544be0c82bc687f5ad1387d
-size 3911
+oid sha256:0ef23a800d840ea0f40b5fa62afa38acce090445cb3c2a778bc893d3596fde6f
+size 4492
