e81b69fbcf0f540590991f7cfbd1f81827dd0b4d
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-06-05 12:58:01 -0400

    Fixed errors when no window is focused on

--- Assets/Editor/ZoneCreationTool.cs
+++ Assets/Editor/ZoneCreationTool.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:575a4424a9421091a2c4c6784fb7aad0219ba402a5399d51f49cddf9baa193ae
-size 21336
+oid sha256:132f3c00efd674fcd1af60a1a0588654cc0f8640b664ad8ff250fb75fc9eec0b
+size 21373
