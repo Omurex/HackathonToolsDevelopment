9d935817cf44e08e435215e4ac094fd16496276d
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-06-05 14:57:26 -0400

    Making progress with dynamic variables for zones

---
+++ Assets/Scripts/DynamicPropertiesGroup.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a5070771cd2f5a4548cefc2f49d59a4d05958a3a4211260478474aa9a1f29d07
+size 3929
---
+++ Assets/Scripts/VariableType.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fc385d4b12086c63f8213deaf991254a452b1cb9ed2350b01ef5f8a46b9684f3
+size 149
