728d046fd5f33a8f80bd5e3e4aaad1f88a42f92e
ethanmbrett <ethanmbrett@gmail.com>
2024-06-07 10:58:58 -0400

    Set up Prefabs.

--- Assets/Scripts/Tile Systems/AHTile.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e13140ed168f02b0064e0fd813b9c4fc35d46f36322480fa7ed1815268e915ca
-size 9222
--- Assets/Scripts/Tile Systems/AHTileSelector.cs
+++ Assets/Scripts/Tile Systems/AHTileSelector.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d00e00d0c229b312df66ac18c427b2859a67eff123a92bb3392f57ba7661aa6
-size 1359
+oid sha256:9722b21bc24b7410b4f4eaa2f65bd88cb75561d2911bc27363186420061c6286
+size 1093
