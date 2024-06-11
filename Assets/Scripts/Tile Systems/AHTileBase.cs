b65e26239071592f22eb7f937808aec1f154af9b
ethanmbrett <ethanmbrett@gmail.com>
2024-06-11 09:55:01 -0400

    Final Updates on Tile Systems

--- Assets/Scripts/Tile Systems/AHTileBase.cs
+++ Assets/Scripts/Tile Systems/AHTileBase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36824b16b5bfdf61b42b0b81e86e4fd48cb3a97165519b35cfe7f9c7acc441d2
-size 5369
+oid sha256:44a0fda8af2821488e9f99c8e2decc00bd165b08a64ffb960be0531cdbc47b8a
+size 5555
--- Assets/Scripts/Tile Systems/AHTileSelector.cs
+++ Assets/Scripts/Tile Systems/AHTileSelector.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9722b21bc24b7410b4f4eaa2f65bd88cb75561d2911bc27363186420061c6286
-size 1093
+oid sha256:5b5ad0cd95ae50f1665ca61176d126fcd77bf35c944c37038d4383b0d395e5e6
+size 1061
