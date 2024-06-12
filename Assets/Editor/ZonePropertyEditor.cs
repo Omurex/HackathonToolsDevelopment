1ffa3066495578b8782228b6d5f2645afdf25e13
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-06-12 19:07:39 -0400

    Added remove button to zone creation

--- Assets/Editor/ZonePropertyEditor.cs
+++ Assets/Editor/ZonePropertyEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e8a29eacb755b30ca80aaf1e0842f9380348cfc12e9befc2e0f905a9ac6ca40
-size 3153
+oid sha256:7c6d7645c1f864d39afca2a9fb1228ece92eb4b65e4f5e939bfa8a21bbe9b945
+size 3143
