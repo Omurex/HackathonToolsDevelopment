56b1c00280f3cb9ac5ddb753972b97db4dd8780d
ethanmbrett <ethanmbrett@gmail.com>
2024-06-12 20:50:16 -0400

    AHTileBase BugFix
    Fixed an issue with additive multipliers where they were added more than once. Fixed an issue with saturation functions that kept growth locked when the saturation function was non-linear.

--- Assets/Scripts/Tile Systems/AHTileBase.cs
+++ Assets/Scripts/Tile Systems/AHTileBase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44a0fda8af2821488e9f99c8e2decc00bd165b08a64ffb960be0531cdbc47b8a
-size 5555
+oid sha256:bcce71d3d5a306e181a0e58130503311766ccba157a0ae5b4724874cec299e89
+size 5667
