7261c10afb25806a20b0989683c473737011b53d
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-06-12 22:08:45 -0400

    Added some comments

--- Assets/Scripts/Tile Systems/AHTileBase.cs
+++ Assets/Scripts/Tile Systems/AHTileBase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad6de363f0e19e5ae222cb746a0ce9bbb89f3372657ce87f00d7abcd2704f601
-size 7576
+oid sha256:78074c2fe970e49fb7eabdb653341052794699d42583eef367e62671013ecad8
+size 7738
